9f3187407a59f842b2f5fa128071f700114c19d5
Fire2710 <58132735+Fire2710@users.noreply.github.com>
2020-06-30 14:01:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,7 +12,16 @@ client.on('ready', () => {
     }
   if (message.content === 'bruh') {
       message.channel.send("<:bruh:726217332632059937>");
-   }
+    }
+  if (message.content === prefix + "help") {
+    var embed = new Discord.RichEmbed()
+        .setTitle("Erreur")
+        .setDescription("<:4077_warning:702240781741719603> une erreur s'est produite pendant l'execution de cette commande !")
+        .addField("Informations supplémentaires", "La commande que vous avez essayer d'utiliser n'est pas encore prête à l'usage !")
+        .setColor('EE3F3F')
+        .setFooter(`Requête envoyer par ${message.author}`)
+      message.channel.send(embed);
+  }
   });
 
 client.login(process.env.TOKEN);
