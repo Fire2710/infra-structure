bad0203eafd4fcb7d2350e793e56f8161ad76d73
Fire2710 <58132735+Fire2710@users.noreply.github.com>
2020-07-01 15:50:41 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,27 +1,32 @@
-const { Client, MessageEmbed } = require('discord.js');
-const client = new Client();
+const fs = require('fs');
+const { Client, MessageEmbed, Collection } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
 const prefix = ".";
 
 // Create an instance of a Discord client
+const client = new Client();
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+  console.log(client.commands);
+}
+
 client.on('ready', () => {
     console.log('I am ready!');
   });
   
   client.on('message', message => {
-    if (message.content === prefix + "avatar") {
-      message.channel.send(message.author.displayAvatarURL());
-    }
-  if (message.content === 'bruh') {
-      message.channel.send("<:bruh:726217332632059937>");
-    }
-  if (message.content === prefix + "help") {
-      const embed = new MessageEmbed()
-        .setTitle("Erreur")
-        .setDescription(":warning: une erreur s'est produite pendant l'execution de cette commande !")
-        .addField("Informations supplémentaires", `La commande que vous avez essayer d'utiliser n'est pas encore prête à l'usage !\n Requête envoyé par **${message.author}**`)
-        .setColor(0xEE3F3F)
-      message.channel.send(embed);
-  }
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(PREFIX.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+    client.commands.get(command).execute(message, args);
   });
 
 client.login(process.env.TOKEN);
